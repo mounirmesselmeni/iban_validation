63abb4906acc807853c6ae42a93c54ae77ad3070
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2014-10-23 22:10:05 +0100

    Using single field with OR validation (SWIFT or IBAN) after verifying requirements email 'accepts an IBAN or BIC and validates them/it'

--- iban_validation/forms.py
+++ iban_validation/forms.py
@@ -1,8 +1,43 @@
 from django import forms
-from django_iban.forms import IBANFormField, SWIFTBICFormField
+from django.core.exceptions import ValidationError
+
+from django_iban.validators import IBANValidator, swift_bic_validator
+
+
+class IbanOrSwiftField(forms.CharField):
+
+    def __init__(self, use_nordea_extensions=False, include_countries=None, *args, **kwargs):
+        self.default_validators = [
+            IBANValidator(use_nordea_extensions, include_countries),
+            swift_bic_validator]
+        super(IbanOrSwiftField, self).__init__(*args, **kwargs)
+
+    def run_validators(self, value):
+        if value in self.empty_values:
+            return
+        errors = []
+        iban_valid = False
+        swift_valid = False
+        err_msg = "Invalid data, only IBAN or SWIFT are acceptable"
+        for v in self.validators:
+            try:
+                v(value)
+                if isinstance(v, IBANValidator):
+                    iban_valid = True
+                elif hasattr(v, '__name__') and v.__name__ == 'swift_bic_validator':
+                    swift_valid = True
+            except ValidationError as e:
+                if hasattr(e, 'code') and e.code in self.error_messages:
+                    e.message = self.error_messages[e.code]
+                if isinstance(v, IBANValidator) or hasattr(v, '__name__') and v.__name__ == 'swift_bic_validator':
+                    if err_msg not in errors:
+                        errors.append(err_msg)
+                else:
+                    errors.extend(e.error_list)
+        if errors and (not swift_valid and not iban_valid):
+            raise ValidationError(errors)
 
 
 class IbanSwiftForm(forms.Form):
 
-    iban = IBANFormField()
-    swift_bic = SWIFTBICFormField()
+    iban_or_swift = IbanOrSwiftField()
--- iban_validation/tests.py
+++ iban_validation/tests.py
@@ -5,18 +5,22 @@ from .forms import IbanSwiftForm
 
 class IbanSwiftValidationTestCase(TestCase):
 
-    def test_wrong_data(self):
-        form_data = {
-            'iban': 'ddsssssfffff',
-            'swift_bic': '545454sdssd54451121'
-        }
+    def test_wrong_iban(self):
+        form_data = {'iban_or_swift': 'ddsssssfffff'}
         form = IbanSwiftForm(data=form_data)
         self.assertEqual(form.is_valid(), False)
 
-    def test_valid_data(self):
-        form_data = {
-            'iban': 'BE62510007547061',
-            'swift_bic': 'BOFAUS3N'
-        }
+    def test_wrong_swift(self):
+        form_data = {'iban_or_swift': '545454sdssd54451121'}
+        form = IbanSwiftForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_valid_iban(self):
+        form_data = {'iban_or_swift': 'BE62510007547061'}
+        form = IbanSwiftForm(data=form_data)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_valid_swift(self):
+        form_data = {'iban_or_swift': 'BOFAUS3N'}
         form = IbanSwiftForm(data=form_data)
         self.assertEqual(form.is_valid(), True)
